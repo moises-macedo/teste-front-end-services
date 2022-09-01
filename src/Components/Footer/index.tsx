d223ab0ef4b9e9bb884a38db936d89f60374d9d1
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-09-01 16:19:20 -0300

    feat: criado componente Footer
    criado componente footer com descrição do rodapé

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,34 @@
+import GrupoService from "../../Assets/Img/logo-gruposervices.svg";
+import FooterGruposService from "../../Assets/Img/label-gs.svg";
+import "../../Assets/Styles/Css/footer.css"
+
+export const Footer = () => {
+    return (
+        <footer className="container--footer-bakcground">
+            <section className="container--footer-desciption">
+
+                <div className="footer--description-imgGrupo">
+                    <img src={GrupoService} alt="Logo Grupo Service" />
+                </div>
+                <ul className="footer--description-links">
+                    <li>
+                        <a href="#">Quem somos</a>
+                    </li>
+                    <li>
+                        <a href="#">Polílitca de privacidade</a>
+                    </li>
+                    <li>
+                        <a href="#">Termos de uso</a>
+                    </li>
+                </ul>
+                <div className="footer--description-imgApp">
+                    <img src={FooterGruposService} alt="Plataforma desenvolvida pelo Grupo Services" />
+                </div>
+                <div className="footer--description-reserved">
+                    <h4>Todos os direitos reservados ao Grupo Services</h4>
+                </div>
+            </section>
+
+        </footer>
+    )
+}
